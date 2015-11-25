2a3eae65c677d2dae81f00b95edf7ae6efbf4442
Loren Mucha <lorenmucha@gmail.com>
2015-11-25 14:39:23 +0100

    Simpler Nav Drawer mit Karte

---
+++ app/src/main/java/com/naturpark/MainActivity.java
@@ -0,0 +1,111 @@
+package com.naturpark;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //Defining Variables
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView map = (MapView) findViewById(R.id.mapview);
+        map.setTileSource(TileSourceFactory.MAPQUESTOSM);
+
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+        GeoPoint startPoint = new GeoPoint(51.080414, 10.434239);
+        mapController.setCenter(startPoint);
+        map.setMultiTouchControls(true);
+
+        // Initializing Toolbar and setting it as the actionbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Initializing NavigationView
+        navigationView = (NavigationView)
+
+                findViewById(R.id.navigation_view);
+
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+
+                                                         {
+
+                                                             // This method will trigger on item Click of navigation menu
+                                                             @Override
+                                                             public boolean onNavigationItemSelected(MenuItem menuItem) {
+                                                                 //Checking if the item is in checked state or not, if not make it in checked state
+                                                                 if (menuItem.isChecked())
+                                                                     menuItem.setChecked(false);
+                                                                 else menuItem.setChecked(true);
+                                                                 //Closing drawer on item click
+                                                                 drawerLayout.closeDrawers();
+                                                                 setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+                                                                 //Check to see which item was being clicked and perform appropriate action
+                                                                 switch (menuItem.getItemId()) {
+                                                                     //Replacing the main content with ContentFragment Which is our Inbox View;
+                                                                     case R.id.start:
+                                                                       // fehlt noch.....
+                                                                         return true;
+
+                                                                     default:
+                                                                         // fehlt noch.....
+                                                                         return true;
+                                                                 }
+                                                             }
+                                                         }
+
+        );
+
+        // Initializing Drawer Layout and ActionBarToggle
+        drawerLayout = (DrawerLayout)
+
+                findViewById(R.id.drawer);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+    }
+}
